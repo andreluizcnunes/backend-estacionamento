c67234dd248376a2b549b3a96fa9c76afe773250
Andre Nunes <andre.doctors@gmail.com>
2025-02-07 07:09:08 -0400

    feat: create basic crud of Usuario

--- src/main/java/br/com/estacionamento/api/web/controller/UsuarioController.java
+++ src/main/java/br/com/estacionamento/api/web/controller/UsuarioController.java
@@ -1,9 +1,14 @@
 package br.com.estacionamento.api.web.controller;
 
+import br.com.estacionamento.api.entity.Usuario;
 import br.com.estacionamento.api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/usuarios")
@@ -12,5 +17,27 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.salvar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getbayId(@PathVariable UUID id){
+        Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Usuario> updatePassword(@PathVariable UUID id, @RequestBody Usuario usuario){
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAll(){
+        List<Usuario> users = usuarioService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
 }
