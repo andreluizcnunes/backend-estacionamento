99f74d3c09b2055a6c56fc724f32db78b3cd154d
Andre Nunes <andre.doctors@gmail.com>
2025-02-08 10:24:47 -0400

    feat(UsuarioController): Documentation  Witch SpringDoc OpenAPI and Swagger

--- src/main/java/br/com/estacionamento/api/web/controller/UsuarioController.java
+++ src/main/java/br/com/estacionamento/api/web/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package br.com.estacionamento.api.web.controller;
 
 import br.com.estacionamento.api.entity.Usuario;
 import br.com.estacionamento.api.service.UsuarioService;
+import br.com.estacionamento.api.web.doc.UsuarioDoc;
 import br.com.estacionamento.api.web.dto.usuario.UsuarioCreateDto;
 import br.com.estacionamento.api.web.dto.mapper.UsuarioMapper;
 import br.com.estacionamento.api.web.dto.usuario.UsuarioPasswordDto;
@@ -15,31 +16,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+
 @RestController
 @RequestMapping("api/v1/usuarios")
 @RequiredArgsConstructor
-public class UsuarioController {
+public class UsuarioController implements UsuarioDoc {
 
     private final UsuarioService usuarioService;
 
+    @Override
     @PostMapping
     public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
 
+    @Override
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getbayId(@PathVariable UUID id){
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
     }
 
+    @Override
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable UUID id, @Valid @RequestBody UsuarioPasswordDto dto){
         Usuario user = usuarioService.editarSenha(id, dto.getCurrentPassword(), dto.getNewPassword(), dto.getConfirmPassword());
         return ResponseEntity.noContent().build();
     }
 
+    @Override
     @GetMapping
     public ResponseEntity<List<UsuarioResponseDto>> getAll(){
         List<Usuario> users = usuarioService.buscarTodos();
---
+++ src/main/java/br/com/estacionamento/api/web/doc/UsuarioDoc.java
@@ -0,0 +1,140 @@
+package br.com.estacionamento.api.web.doc;
+
+import br.com.estacionamento.api.web.dto.usuario.UsuarioCreateDto;
+import br.com.estacionamento.api.web.dto.usuario.UsuarioPasswordDto;
+import br.com.estacionamento.api.web.dto.usuario.UsuarioResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
+
+@Tag(
+        name = "Usuarios",
+        description = "Contém todas as operações para gerenciar os usuários do sistema."
+)
+public interface UsuarioDoc {
+
+    @Operation(
+            summary = "Cria um novo usuário",
+            description = "Recurso para criar um novo usuário no sistema.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Usuário criado com sucesso.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UsuarioResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "409",
+                            description = "Usuário e-mail já cadastrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Recurso não processado por dados de entrada invalidos.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto);
+
+
+    @Operation(
+            summary = "Recuperar um usuário por ID",
+            description = "Recuperar um usuário por ID.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Recurso recuperado com sucesso.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UsuarioResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<UsuarioResponseDto> getbayId(@PathVariable UUID id);
+
+
+    @Operation(
+            summary = "Atualizar a senha.",
+            description = "Atualizar a senha.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Senha atualizada com sucesso.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Void.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Recurso não encontrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Senha não confere",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<Void> updatePassword(@PathVariable UUID id, @Valid @RequestBody UsuarioPasswordDto dto);
+
+    @Operation(
+            summary = "Lista todos os usuários.",
+            description = "Lista todos os usuários cadastrados no sistema.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Lista todos os usuários cadastrados no sistema.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = UsuarioResponseDto.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Nenhum usuário encontrado.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponse.class)
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<List<UsuarioResponseDto>> getAll();
+}
